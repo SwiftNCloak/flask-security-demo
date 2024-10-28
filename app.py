7b32b38e5ad21e1e171354843d6f848af7401e59
SwiftNCloak <markyjamesbonifacio@gmail.com>
2024-10-29 01:41:47 +0800

    some stuff

--- app.py
+++ app.py
@@ -1,39 +1,45 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SECRET_KEY'] = 'supersecretkey'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+migrate = Migrate(app, db)
 
 class User(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))  # Storing plaintext password
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            return redirect(url_for('home'))
+        else:
+            return 'Invalid username or password'
     return render_template('index.html')
 
-# Vulnerable Version
 @app.route('/register', methods=['GET', 'POST'])
-def register_vulnerable():
+def register():
     if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        # Vulnerable Version:
-        # This version does NOT hash the password and is insecure.
-        # Storing plaintext passwords directly is a major security risk.
-        vulnerable_user = User(username=username, password=password)  # Storing plaintext password
-        db.session.add(vulnerable_user)
+        username = request.form['username']
+        password = request.form['password']
+        user = User(username=username, password=password)
+        db.session.add(user)
         db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('register.html')
 
-        return jsonify({"message": "User registered successfully (vulnerable)!"})
-
-    return render_template('register.html', version='vulnerable')
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
